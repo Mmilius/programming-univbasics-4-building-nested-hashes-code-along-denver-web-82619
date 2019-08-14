e9f4fff29786de227e0dea1ca0781263606313e5
Martin Milius <martin.milius@gmail.com>
2019-08-14 01:48:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,7 +22,7 @@ def third_challenge
       :heroine => {
         :name => "Juliet", :age => "15", :status => "alive"
         },
-      :herorine_friends => []
+      :heroine_friends => []
    }
   }
 
